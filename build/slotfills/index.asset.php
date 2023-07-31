97e6354068947dd1d25337f89aeb5e64c617b067
anubisthejackle <anubisthejackle@users.noreply.github.com>
2023-07-31 14:53:34 +0000

    Changes from 36881b80ec567793a84900512493abdb8921e006

---
+++ build/slotfills/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '9389177fce509bf988f8');
